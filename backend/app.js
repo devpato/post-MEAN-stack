84bd6c4505d0f3234fc6b480d671fa608d307ab3
Patricio <wearetamo@gmail.com>
2019-06-08 21:49:37 -0400

    api for post

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const EXPRESS = require('express');
+const uuidv4 = require('uuid/v4');
 
 const APP = EXPRESS();
 
-APP.use((req, res, next) => {
-  console.log('First Middleware');
-  next();
-});
-
-APP.use((req, res, next) => {
-  res.send('Hello from express!');
+APP.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: uuidv4(),
+      title: 'MEAN app from scratch',
+      content: 'This is the content of the first post'
+    }
+  ];
+  res.status(200).json({
+    message: 'Post fectch successfully',
+    posts: posts
+  });
 });
 
 module.exports = APP;

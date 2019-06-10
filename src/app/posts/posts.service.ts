5b4a4c1cfe13cab2e977e7fa08b812e80c8cc684
Patricio <wearetamo@gmail.com>
2019-06-10 11:43:47 -0400

    updated getPost()

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-
+import { Subject, Observable } from 'rxjs';
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
+import { error } from 'util';
+import { stringify } from '@angular/compiler/src/util';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
+  BASE_URL = 'http://localhost:5000/api';
+
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    return [...this.posts];
+    return this.http.get<{ message: string; posts: Post[] }>(
+      this.BASE_URL + '/posts'
+    );
   }
 
   getPostUpdateListener() {
@@ -17,7 +24,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = { id: null, title: title, content: content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }

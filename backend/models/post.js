7347911b894278eefce92d2a7778b33b60f43d39
Patricio <wearetamo@gmail.com>
2019-06-17 00:04:27 -0400

    created schema

---
+++ backend/models/post.js
@@ -0,0 +1,8 @@
+const MONGOOSE = require('mongoose');
+
+const POST_SCHEMA = MONGOOSE.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true }
+});
+
+module.exports = MONGOOSE.model('Post', POST_SCHEMA);

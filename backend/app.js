4f001afca69fccf7048d3e069379869b3a2c0422
Patricio <wearetamo@gmail.com>
2019-06-21 00:16:14 -0400

    injected POSTS routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 const EXPRESS = require('express');
-const uuidv4 = require('uuid/v4');
 const BODY_PARSER = require('body-parser');
-const POST = require('./models/post');
 const APP = EXPRESS();
 const MONGOOSE = require('mongoose');
+const POSTS_ROUTES = require('./routes/posts');
 
 MONGOOSE.connect(
   'mongodb+srv://super:super@cluster0-fnddg.mongodb.net/node-angular?retryWrites=true&w=majority',
@@ -29,57 +28,5 @@ APP.use((req, res, next) => {
   next();
 });
 
-APP.post('/api/posts', (req, res, next) => {
-  const NEW_POST = new POST({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  NEW_POST.save().then(result => {
-    res.status(201).json({
-      message: 'Post added succesfully',
-      id: result._id
-    });
-  });
-});
-
-APP.put('/api/posts/:id', (req, res, next) => {
-  const UPDATED_POST = new POST({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  POST.updateOne({ _id: req.params.id }, UPDATED_POST).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Update Successful' });
-  });
-});
-
-APP.get('/api/posts', (req, res, next) => {
-  POST.find().then(documents => {
-    res.status(200).json({
-      message: 'Post fectch successfully',
-      posts: documents
-    });
-  });
-});
-
-APP.get('/api/posts/:id', (req, res, next) => {
-  POST.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(400).json({ message: 'Post not found!' });
-    }
-  });
-});
-
-APP.delete('/api/posts/:id', (req, res, next) => {
-  POST.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post deleted' });
-  });
-});
-
+APP.use('/api/posts', POSTS_ROUTES);
 module.exports = APP;

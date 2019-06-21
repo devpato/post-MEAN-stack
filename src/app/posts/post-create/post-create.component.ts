37c923849dc9f24fe2c0ae01b31ebe7738081336
Patricio <wearetamo@gmail.com>
2019-06-20 22:28:27 -0400

    subscribing to to get iteam

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
   post: Post;
+  loading = true;
 
   constructor(
     public postsService: PostsService,
@@ -27,7 +28,9 @@ export class PostCreateComponent implements OnInit {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(post => {
+          this.post = post;
+        });
       } else {
         this.mode = 'create';
         this.postId = null;

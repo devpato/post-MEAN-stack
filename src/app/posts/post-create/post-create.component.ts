64de66f3682147ad2d7434d1fe7fdf1089ae1a62
Patricio <wearetamo@gmail.com>
2019-06-23 17:20:15 -0400

    update creation component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -46,7 +46,11 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
       this.form.reset();
     } else {
       console.log('update');

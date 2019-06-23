677ff587dfd8ab0c0dd55f76a8e87bbf7d8faa89
Patricio <wearetamo@gmail.com>
2019-06-23 18:46:50 -0400

    fixed model schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -2,7 +2,8 @@ const MONGOOSE = require('mongoose');
 
 const POST_SCHEMA = MONGOOSE.Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true }
+  content: { type: String, required: true },
+  imagePath: { type: String, required: true }
 });
 
 module.exports = MONGOOSE.model('Post', POST_SCHEMA);

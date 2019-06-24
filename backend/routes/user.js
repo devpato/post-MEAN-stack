36fe53a0f5428e68bcc3073d10886ea67fbad117
Patricio <wearetamo@gmail.com>
2019-06-24 02:31:13 -0400

    fixed user router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,24 +5,24 @@ const USER = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(hash => {
-    const USER = new User({
+    const NEW_USER = new USER({
       email: req.body.email,
       password: hash
     });
-  });
 
-  USER.save().then(result => {
-    res
-      .status(201)
-      .json({
-        message: 'User created!',
-        result: result
-      })
-      .catch(err => {
-        res.status(500).json({
-          error: err
+    NEW_USER.save().then(result => {
+      res
+        .status(201)
+        .json({
+          message: 'User created!',
+          result: result
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
         });
-      });
+    });
   });
 });
 

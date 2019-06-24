50e6422bf7f4b021f8553ce30a2737a6545b4900
Patricio <wearetamo@gmail.com>
2019-06-24 02:26:11 -0400

    added unique validator

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,11 @@
 const MONGOOSE = require('mongoose');
+const UNIQUE_VALIDATOR = require('mongoose-unique-validator');
 
 const USER_SCHEMA = MONGOOSE.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
+
+USER_SCHEMA.plugin(UNIQUE_VALIDATOR);
 
 module.exports = MONGOOSE.model('User', USER_SCHEMA);

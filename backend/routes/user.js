803626a6a0156e11c58dc3131920300d2d8a7c00
Patricio <wearetamo@gmail.com>
2019-06-24 02:26:21 -0400

    hashed password with bycript

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const USER = require('../models/user');
 
-router.post('/signup', (req, res, next) => {});
+router.post('/signup', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const USER = new User({
+      email: req.body.email,
+      password: hash
+    });
+  });
+
+  USER.save().then(result => {
+    res
+      .status(201)
+      .json({
+        message: 'User created!',
+        result: result
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
+});
 
 module.exports = router;

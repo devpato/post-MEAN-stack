bd43f213554154b6da6d7f7b7addfa1860e07872
Patricio <wearetamo@gmail.com>
2019-06-26 01:47:54 -0400

    login api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -27,32 +27,37 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
-  USER.find({ email: req.body.email })
+  let fetchedUser;
+  USER.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
         return res.status(401).json({
-          message: 'Auth failed!'
+          message: 'Auth failed'
         });
       }
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
       if (!result) {
         return res.status(401).json({
-          message: 'Auth failed!'
+          message: 'Auth failed'
         });
       }
-
-      const TOKEN = JWT.sign(
-        { email: user.email, userId: user._id },
+      const token = JWT.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
         'secret_this_should_be_longer',
         { expiresIn: '1h' }
       );
+      res.status(200).json({
+        token: token
+      });
     })
     .catch(err => {
       return res.status(401).json({
-        message: 'Auth failed!'
+        message: 'Auth failed'
       });
     });
 });
+
 module.exports = router;

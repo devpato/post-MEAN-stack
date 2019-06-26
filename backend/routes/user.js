721a229e79c8f8316778483b2b2a55fb73bd3edf
Patricio <wearetamo@gmail.com>
2019-06-26 01:30:29 -0400

    added login route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const USER = require('../models/user');
+const JWT = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(hash => {
@@ -23,5 +24,35 @@ router.post('/signup', (req, res, next) => {
         });
       });
   });
+});
+
+router.post('/login', (req, res, next) => {
+  USER.find({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed!'
+        });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed!'
+        });
+      }
+
+      const TOKEN = JWT.sign(
+        { email: user.email, userId: user._id },
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' }
+      );
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth failed!'
+      });
+    });
 });
 module.exports = router;

91fa1f4a948a373b89e03fe7615adb142644d961
Patricio <wearetamo@gmail.com>
2019-06-27 01:35:06 -0400

    added authorization header

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ APP.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', '*');
   next();

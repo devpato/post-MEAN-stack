5f0be67d2e429e0fe88199015c7e1b58a61eb9d1
Patricio <wearetamo@gmail.com>
2019-06-27 01:33:34 -0400

    updated post schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -3,8 +3,8 @@ const MONGOOSE = require('mongoose');
 const POST_SCHEMA = MONGOOSE.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  imagePath: { type: String, required: true },
-  creator: { type: MONGOOSE.Schema.Types.ObjectId, ref: 'User', required: true }
+  imagePath: { type: String, required: true }
+  //creator: { type: MONGOOSE.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 module.exports = MONGOOSE.model('Post', POST_SCHEMA);

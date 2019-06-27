fc446f9e556b8a913ab299f057985839fda638b1
Patricio <wearetamo@gmail.com>
2019-06-27 01:35:59 -0400

    using interceptor in the FE

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  private token: string;
   BASE_URL = 'http://localhost:5000/api/user';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     console.log(authData);
@@ -19,8 +23,17 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     console.log(authData);
-    this.http.post(this.BASE_URL + '/login', authData).subscribe(response => {
-      console.log(response);
-    });
+    this.http
+      .post<{ token: string }>(this.BASE_URL + '/login', authData)
+      .subscribe(response => {
+        console.log(response.token);
+        this.token = response.token;
+        this.router.navigate(['/']);
+      });
+  }
+
+  getToken(): string {
+    console.log(this.token);
+    return this.token;
   }
 }

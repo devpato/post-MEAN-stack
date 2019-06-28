c97123c05be4818b773400562fc752389dba2f43
Patricio <wearetamo@gmail.com>
2019-06-28 00:38:22 -0400

    auth guard created

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import {
+  CanActivate,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    const IS_AUTH = !this.authService.getIsAuth();
+    if (!IS_AUTH) {
+      this.router.navigate(['login']);
+    }
+
+    return IS_AUTH;
+  }
+}

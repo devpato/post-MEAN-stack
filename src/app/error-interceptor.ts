5cb8db399b12ba8aca5ce5fbc4a914d6e052a397
Patricio <wearetamo@gmail.com>
2019-06-29 16:10:26 -0400

    sending error to dialog

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -6,11 +6,23 @@ import {
 } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ErrorComponent } from './error/error.component';
+
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private dialog: MatDialog) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error.error.error.message);
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error.error.message) {
+          errorMessage = error.error.error.message;
+        }
+        this.dialog.open(ErrorComponent, {
+          data: { message: errorMessage }
+        });
         return throwError(error);
       })
     );

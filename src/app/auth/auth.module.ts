27283e5917594c8e407262b9b8a8a662a41f6f55
Patricio <wearetamo@gmail.com>
2019-06-29 18:13:32 -0400

    injected auth routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { SignupComponent } from './signup/signup.component';
 import { AngulartMaterialModule } from '../angular-material.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { AuthRoutingModule } from './auth.routing.module';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, AngulartMaterialModule, FormsModule]
+  imports: [
+    CommonModule,
+    AngulartMaterialModule,
+    FormsModule,
+    AuthRoutingModule
+  ]
 })
 export class AuthModule {}

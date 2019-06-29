98c622c33e894e1b47e2996cbf1346e92630be5c
Patricio <wearetamo@gmail.com>
2019-06-29 01:49:23 -0400

    adde clg to interceptor

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -12,7 +12,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = this.authService.getToken();
-    console.log(authToken);
+    console.log('Bearer ' + authToken);
     const authRequest = req.clone({
       headers: req.headers.set('Authorization', 'Bearer ' + authToken)
     });

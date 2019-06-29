760051cf2412badd9396a618962ee0fcd6c16aea
Patricio <wearetamo@gmail.com>
2019-06-29 17:41:27 -0400

    created angular material module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,25 @@
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule,
+  MatDialogModule
+} from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  exports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule
+  ]
+})
+export class AngulartMaterialModule {}

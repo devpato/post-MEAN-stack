fa47e5a4acd48df22e6aec6bead5c094d3ade192
Patricio <wearetamo@gmail.com>
2019-06-29 16:27:56 -0400

    handeling error int he interceptor

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -17,8 +17,8 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'An unknown error occurred!';
-        if (error.error.error.message) {
-          errorMessage = error.error.error.message;
+        if (error.error.message) {
+          errorMessage = error.error.message;
         }
         this.dialog.open(ErrorComponent, {
           data: { message: errorMessage }

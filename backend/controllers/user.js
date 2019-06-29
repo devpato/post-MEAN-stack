19fcfcab349d54488d195db38b0621323dd4ade1
Patricio <wearetamo@gmail.com>
2019-06-29 16:39:33 -0400

    user controller created

---
+++ backend/controllers/user.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcrypt');
+const USER = require('../models/user');
+const JWT = require('jsonwebtoken');
+
+exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new USER({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: 'User created!',
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'Invalid authentification credentials!'
+        });
+      });
+  });
+};
+
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  USER.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed, user doesn't exist"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed, cretentials didn't match"
+        });
+      }
+      const token = JWT.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        'secret_this_should_be_longer',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token,
+        expiresIn: 3600,
+        userId: fetchedUser._id
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth failed'
+      });
+    });
+};
